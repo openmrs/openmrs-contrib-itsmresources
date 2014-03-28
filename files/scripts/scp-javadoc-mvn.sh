3ea330fa07532880963a05eed0ca7ba21957693b
Ryan Yates <ryan@openmrs.org>
2014-03-28 16:03:44 -0400

    remove unnecessary mkdir

--- files/scripts/scp-javadoc-mvn.sh
+++ files/scripts/scp-javadoc-mvn.sh
@@ -5,6 +5,5 @@ builddir=$1
 version=$2
 mkdir -p ${builddir}
 mkdir -p ${builddir}/target/apidocs
-mkdir -p ${builddir}/target/$2
 mv ${builddir}/target/apidocs ${builddir}/target/$2
 scp -r -P 22 ${builddir}/target/$2 ${targetuser}@${targethost}:${targetpath}/
