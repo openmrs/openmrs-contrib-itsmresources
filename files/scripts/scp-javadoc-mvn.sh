57e98f988eb513e970a554dff135083625b93863
Michael Downey <michael@openmrs.org>
2014-03-28 15:22:48 -0400

    Additional updates for Bamboo remote agents
    Need to pass working dir from Bamboo, updating script

--- files/scripts/scp-javadoc-mvn.sh
+++ files/scripts/scp-javadoc-mvn.sh
@@ -1,10 +1,10 @@
 targethost=bafoussam.openmrs.org
 targetuser=bamboo
 targetpath=/opt/www/resources.openmrs.org
-builddir=/opt/bamboo-home/xml-data/build-dir/$1
-VERSION=$3
+builddir=$1/$2
+version=$3
 mkdir -p ${builddir}
 mkdir -p ${builddir}/target/apidocs
-mkdir -p ${builddir}/target/$2
-mv ${builddir}/target/apidocs ${builddir}/target/$2
-scp -r -P 22 ${builddir}/target/$2 ${targetuser}@${targethost}:${targetpath}/
+mkdir -p ${builddir}/target/$3
+mv ${builddir}/target/apidocs ${builddir}/target/$3
+scp -r -P 22 ${builddir}/target/$3 ${targetuser}@${targethost}:${targetpath}/
