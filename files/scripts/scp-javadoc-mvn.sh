aca62823b8d26c4a9bfe1747cb5021e04f8db4b2
Michael Downey <michael@openmrs.org>
2014-03-28 14:13:49 -0400

    Updating from bafang to bafoussam
    New server

--- files/scripts/scp-javadoc-mvn.sh
+++ files/scripts/scp-javadoc-mvn.sh
@@ -1,4 +1,4 @@
-targethost=bafang.openmrs.org
+targethost=bafoussam.openmrs.org
 targetuser=bamboo
 targetpath=/opt/www/resources.openmrs.org
 builddir=/opt/bamboo-home/xml-data/build-dir/$1
@@ -7,4 +7,4 @@ mkdir -p ${builddir}
 mkdir -p ${builddir}/target/apidocs
 mkdir -p ${builddir}/target/$2
 mv ${builddir}/target/apidocs ${builddir}/target/$2
-scp -r -P 24242 ${builddir}/target/$2 ${targetuser}@${targethost}:${targetpath}/
+scp -r -P 22 ${builddir}/target/$2 ${targetuser}@${targethost}:${targetpath}/
