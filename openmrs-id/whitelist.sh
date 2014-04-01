01dc65d2930cc49a9728940e6b01a181c0aeb164
Michael Downey <michael@openmrs.org>
2014-04-01 18:40:09 -0400

    Update to grep log

--- openmrs-id/whitelist.sh
+++ openmrs-id/whitelist.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-read -p "Enter IP address to add to Dashboard whitelist: " ip_addr
+read -p "Enter IP address in question: " ip_addr
 echo
+echo "If the IP address was attempted to be used, but was flagged, it will appear on 1 or more following lines:"
+grep "botproof - IP address $ip_addr" /opt/id/logs/openmrsid.log
+echo
+echo "If you do not see a log entry above, the IP address was not flagged."
 read -p "Press [Enter] key to type the MySQL root password add IP address $ip_addr to the whitelist, or CTRL-C to quit...."
 mysql -u root -p -e "INSERT INTO id_dashboard.IPWhitelists (id, address, createdAt, updatedAt) VALUES (NULL, '$ip_addr', NOW(), NOW());"
 echo "Process complete. You may wish to check the IPWhitelists table to verify."
