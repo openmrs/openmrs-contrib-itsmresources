d87ff3f2977cdf09ceaff8db5c4ea65bcfe75b85
Ryan Yates <ryan@openmrs.org>
2014-04-07 14:03:50 -0400

    changing deploy folder
    Hopefully will get around the mv wonkiness

--- files/scripts/scp-javadoc-mvn.sh
+++ files/scripts/scp-javadoc-mvn.sh
@@ -3,7 +3,7 @@ targetuser=bamboo
 targetpath=/opt/www/resources.openmrs.org
 builddir=$1
 version=$2
-mkdir -p ${builddir}
+mkdir -p ${builddir}/deploy
 mkdir -p ${builddir}/target/apidocs
-mv ${builddir}/target/apidocs ${builddir}/target/$2
-scp -r -P 22 ${builddir}/target/$2 ${targetuser}@${targethost}:${targetpath}
+mv ${builddir}/target/apidocs ${builddir}/deploy/$2
+scp -r -P 22 ${builddir}/deploy/$2 ${targetuser}@${targethost}:${targetpath}
