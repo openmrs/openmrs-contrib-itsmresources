e78f8b1a93a64327248a20003d07e96686bf642d
Ryan Yates <ryan@openmrs.org>
2014-04-09 10:24:05 -0400

    Simplifying code

--- files/scripts/scp-nightlybuild.sh
+++ files/scripts/scp-nightlybuild.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
+# Variables that are passed from the bamboo deploy plan. e.g. ${bamboo.build.working.directory} trunk
 builddir=$1
-domain=216.70.68.11:8080
-domainwithoutport=${domain%%:*}
-tomcatuser=openmrs
-tomcatpass=rubbed-welcome-describe-tide
-webapp=openmrs
 branchname=$2
 
 
 # make /dist/nightly
-mkdir -p ${builddir}/dist
 nightlydir=${builddir}/dist/nightly
 mkdir -p ${nightlydir}
 
@@ -21,22 +16,8 @@ nightlydatedirwithbranch=${nightlydatedir}/$2
 mkdir ${nightlydatedirwithbranch}
 
 # copy .war files
-cp ${builddir}/webapp/target/openmrs.war ${nightlydatedirwithbranch}
-cp ${builddir}/webapp/target/openmrs/WEB-INF/lib/openmrs-api-*.jar ${nightlydatedirwithbranch}
+cp ${builddir}/openmrs.war ${nightlydatedirwithbranch}
+cp ${builddir}/openmrs-api-*.jar ${nightlydatedirwithbranch}
 
 # Rsync nightly build to sourceforge
 rsync -avOP -e 'ssh -i /home/bamboo/bamboo-ssh-deploy/id_rsa' ${nightlydir} openmrs,openmrsdev@frs.sourceforge.net:/home/frs/project/o/op/openmrs/
-
-#echo Undeploying the $webapp webapp on $domain
-#curl -silent -u ${tomcatuser}:${tomcatpass} -o ${builddir}/webapp/target/tomcat.output.undeploy http://${domain}/manager/html/undeploy?path=/$webapp
-#wait
-
-#echo Dropping and recreating the database
-#ssh -i /home/bamboo/.ssh/id_rsa bamboo@sprint1.openmrs.org "mysql -uopenmrs-ci -p8Mt3EhxpxQwYnPhZYRgymiQqNXue0rO -e'drop database openmrs; create database openmrs default character set utf8;'"
-
-#echo Loading database demo data
-#ssh -i /home/bamboo/.ssh/id_rsa bamboo@sprint1.openmrs.org "mysql -uopenmrs-ci -p8Mt3EhxpxQwYnPhZYRgymiQqNXue0rO -Dopenmrs < /home/bamboo/Demo-1.8.0.sql"
-
-#echo Uploading $webapp.war to tomcat on $domain
-#curl -silent -u ${tomcatuser}:${tomcatpass} -o ${builddir}/webapp/target/tomcat.output.upload -F deployWar=@${nightlydatedir}/$webapp.war --retry 10 http://${domain}/manager/html/upload
-#wait
