f7ef338f6c2e9a7faee6bf6d712e4a77dc6d1fcf
Robert O'Connor <rob@oconnor.ninja>
2016-08-15 03:27:22 -0400

    Adjust the default values a bit; install docker-compose

--- openmrs-modules/profiles/manifests/docker.pp
+++ openmrs-modules/profiles/manifests/docker.pp
@@ -2,6 +2,13 @@ class profiles::docker (
   $users,
 ){
   class { '::docker':
-    docker_users => $users,
+    docker_users    => $users,
+    dns             => ['8.8.8.8','8.8.4.4'],
+    iptables        => false,
+    version         => 'latest',
+  }
+  class { 'docker::compose':
+    ensure  => 'present',
+    version => '1.8.0',
   }
 }
