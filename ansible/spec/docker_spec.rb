ec764ca8f5867e98e24356d07755ebf4b93e0463
Robert O'Connor <rob@oconnor.ninja>
2017-01-07 17:14:35 -0500

    Ran rubocop on the docker_spec and Vagrantfile to lint

--- ansible/spec/docker_spec.rb
+++ ansible/spec/docker_spec.rb
@@ -1,13 +1,13 @@
 require_relative 'spec_helper'
 
 describe 'docker machine' do
-  it "should have docker command" do
-      expect(command('docker version').stdout).to include('1.12.5')
+  it 'should have docker command' do
+    expect(command('docker version').stdout).to include('1.12.5')
   end
-  it "should have docker on overlay2" do
-      expect(command('docker info').stdout).to include('Storage Driver: overlay2')
+  it 'should have docker on overlay2' do
+    expect(command('docker info').stdout).to include('Storage Driver: overlay2')
   end
-  it "should have docker-compose command" do
-      expect(command('docker-compose version').stdout).to include('1.9.0')
+  it 'should have docker-compose command' do
+    expect(command('docker-compose version').stdout).to include('1.9.0')
   end
 end
