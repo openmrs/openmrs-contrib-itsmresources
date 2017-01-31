beb9fea970950537dabdb456f8ba587c581ff459
Robert O'Connor <rob@oconnor.ninja>
2017-01-31 18:01:59 -0500

    tweak the mongo-backup script

--- scripts/mongo-backup.sh
+++ scripts/mongo-backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Simple script to backup mongodb nightly
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-BACKUP_PATH=/var/backups/mongodb/${TIMESTAMP}
+BACKUP_PATH="/var/backups/mongodb/${TIMESTAMP}"
 
 mkdir -p "${BACKUP_PATH}"
 # backup the database
-/usr/bin/nice mongodump -u "${MONGO_USERNAME}" -p "${MONGO_PASSWORD}" -d openmrsid -out"${BACKUP_PATH}"
+/usr/bin/nice mongodump -u "${MONGO_USERNAME}" -p "${MONGO_PASSWORD}" -d openmrsid -out "${BACKUP_PATH}"
 chmod 640 -R "${BACKUP_PATH}"
