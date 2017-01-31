6ebece80ed800024f675e985bd45c10b4625db6f
Robert O'Connor <rob@oconnor.ninja>
2017-01-31 16:45:50 -0500

    Also quote env vars in LDAP backup script

--- scripts/ldap-backup.sh
+++ scripts/ldap-backup.sh
@@ -4,14 +4,13 @@
 # these up to Amazon Glacier or S3.
 
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-BACKUP_PATH=/var/backups/ldap/${TIMESTAMP}
-
-mkdir -p ${BACKUP_PATH}
+BACKUP_PATH="/var/backups/ldap/${TIMESTAMP}"
+mkdir -p "${BACKUP_PATH}"
 
 # backup the database
-/usr/bin/nice /usr/sbin/slapcat -n 1 > ${BACKUP_PATH}/openmrs_ldap_backup.ldif
+/usr/bin/nice /usr/sbin/slapcat -n 1 > "${BACKUP_PATH}/openmrs_ldap_backup.ldif"
 
-tar cpjf ${BACKUP_PATH}/etc_ldap.tbz2 /etc/ldap >/dev/null 2>&1
-tar cpjf ${BACKUP_PATH}/var_lib_ldap.tbz2 /var/lib/ldap >/dev/null 2>&1
+tar cpjf "${BACKUP_PATH}/etc_ldap.tbz2" /etc/ldap >/dev/null 2>&1
+tar cpjf "${BACKUP_PATH}/var_lib_ldap.tbz2" /var/lib/ldap >/dev/null 2>&1
 
-chmod 640 -R ${BACKUP_PATH}
+chmod 640 -R "${BACKUP_PATH}"
