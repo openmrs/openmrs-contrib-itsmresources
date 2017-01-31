7369ad7718a677ced41b1696bd65312b6909a8c8
Robert O'Connor <rob@oconnor.ninja>
2017-01-31 16:42:37 -0500

    Double quote env vars

--- scripts/mongo-backup.sh
+++ scripts/mongo-backup.sh
@@ -3,9 +3,7 @@
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
 BACKUP_PATH=/var/backups/mongodb/${TIMESTAMP}
 
-mkdir -p ${BACKUP_PATH}
-
+mkdir -p "${BACKUP_PATH}"
 # backup the database
-/usr/bin/nice mongodump -u $MONGO_USERNAME -p $MONGO_PASSWORD -d openmrsid -out ${BACKUP_PATH}
-
-chmod 640 -R ${BACKUP_PATH}
+/usr/bin/nice mongodump -u "${MONGO_USERNAME}" -p "${MONGO_PASSWORD}" -d openmrsid -out"${BACKUP_PATH}"
+chmod 640 -R "${BACKUP_PATH}"
