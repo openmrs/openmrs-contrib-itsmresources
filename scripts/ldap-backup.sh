8160e9c1941624e19317118d1d34777839a47b10
Robert O'Connor <rob@oconnor.ninja>
2017-02-23 06:45:57 -0500

    Update the scripts for LDAP backup and maintenance.

--- scripts/ldap-backup.sh
+++ scripts/ldap-backup.sh
@@ -8,15 +8,15 @@ BACKUP_PATH="/var/backups/ldap/${TIMESTAMP}"
 mkdir -p "${BACKUP_PATH}"
 
 # backup the config
-/usr/bin/nice /usr/sbin/slapcat -n 0 > "${BACKUP_PATH}/config.ldif"
+/usr/bin/nice /usr/sbin/slapcat -F /opt/id/data/ldap/config -n 0 > "${BACKUP_PATH}/config.ldif"
 
 # backup the database (bring down slapd)
-sudo service slapd stop
-/usr/bin/nice /usr/sbin/slapcat -n 1 > "${BACKUP_PATH}/openmrs_ldap_backup.ldif"
+/usr/bin/nice sudo docker-compose -f /opt/id/docker-compose-prod.yml stop openldap
+/usr/bin/nice /usr/sbin/slapcat -F /opt/id/data/ldap/config -n 1 > "${BACKUP_PATH}/openmrs_ldap_backup.ldif"
 
-/usr/bin/nice tar cpjf "${BACKUP_PATH}/var_lib_ldap.tbz2" /var/lib/ldap >/dev/null 2>&1
+/usr/bin/nice tar cpjf "${BACKUP_PATH}/ldap_database.tbz2" /opt/id/data/ldap/database >/dev/null 2>&1
 # bring slapd back up now
-sudo service slapd start
+/usr/bin/nice sudo docker-compose -f /opt/id/docker-compose-prod.yml start openldap
 
-/usr/bin/nice tar cpjf "${BACKUP_PATH}/etc_ldap.tbz2" /etc/ldap >/dev/null 2>&1
+/usr/bin/nice tar cpjf "${BACKUP_PATH}/ldap_config.tbz2" /opt/id/data/ldap/config >/dev/null 2>&1
 chmod 640 -R "${BACKUP_PATH}"
