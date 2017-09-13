a271afb4108515c47aa541935b0608f684d30f94
Robert O'Connor <rob@oconnor.ninja>
2017-09-13 04:43:41 -0400

    Add logging for backup job uploads

--- ansible/files/backup-upload.sh
+++ ansible/files/backup-upload.sh
@@ -7,7 +7,8 @@ set -x
 VM=${1}
 
 cd /opt/backups/
+echo "Uploading Nightly backups for $(date)" >> /tmp/backups.log
 for file in $(find . -type f); do
-  aws s3 cp --sse "aws:kms" $file s3://openmrs-backups/${VM}/
+  aws s3 cp --sse "aws:kms" $file s3://openmrs-backups/${VM}/ >> /tmp/backups.log
   rm -f $file
 done
