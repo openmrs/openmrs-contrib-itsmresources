43ac02fd4c782718fa99ef408df27438c9e45a64
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-09-15 21:16:10 +1000

    Add backup cron logs to /var/log

--- ansible/files/backup-upload.sh
+++ ansible/files/backup-upload.sh
@@ -6,7 +6,7 @@ set -x
 
 VM=${1}
 for file in $(find /opt/backups -type f); do
-  echo "Uploading Nightly backups for $(date)" >> /tmp/backups.log
-  /usr/local/bin/aws s3 cp --sse "aws:kms" $file s3://openmrs-backups/${VM}/ >> /tmp/backups.log 2>&1
+  echo "Uploading Nightly backups for $(date)"
+  /usr/local/bin/aws s3 cp --sse "aws:kms" $file s3://openmrs-backups/${VM}/
   rm -f $file
 done
