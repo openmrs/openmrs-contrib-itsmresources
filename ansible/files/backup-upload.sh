c8e08bd7668dadaa3fcddedeb31640659d24a4a2
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-09-16 18:56:11 +1000

    Add more logs

--- ansible/files/backup-upload.sh
+++ ansible/files/backup-upload.sh
@@ -5,8 +5,10 @@ set -u
 set -x
 
 VM=${1}
+echo "Uploading Nightly backups for $(date)"
+
 for file in $(find /opt/backups -type f); do
-  echo "Uploading Nightly backups for $(date)"
+  echo "File $file"
   /usr/local/bin/aws s3 cp --sse "aws:kms" $file s3://openmrs-backups/${VM}/
   rm -f $file
 done
