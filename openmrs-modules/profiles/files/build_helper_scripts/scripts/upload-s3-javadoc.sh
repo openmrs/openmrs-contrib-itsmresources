c618ef8784e120df9ccf7a01d0406e108aa3d46a
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-10-15 18:47:12 +1100

    ITSM-4049: Add and comparison for docs upload script

--- openmrs-modules/profiles/files/build_helper_scripts/scripts/upload-s3-javadoc.sh
+++ openmrs-modules/profiles/files/build_helper_scripts/scripts/upload-s3-javadoc.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+virtualenv .env
+. .env/bin/activate
+
 set -e
 set -u
 set -x
 
 targetpath='openmrs-docs'
 
-
 builddir=$1
 version=$2
-
-virtualenv .env
-. .env/bin/activate
 
 pip install awscli
 
 mkdir -p ${builddir}/deploy
 mkdir -p ${builddir}/target/apidocs
 mv ${builddir}/target/apidocs ${builddir}/deploy/${version}
-aws s3 sync ${builddir}/deploy/${version} s3://${targetpath}/${version}
+aws s3 sync --size-only --region us-west-2 ${builddir}/deploy/${version} s3://${targetpath}/${version}
