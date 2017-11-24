2918485d47a399bbee0543ea000b262e74a597cf
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-11-25 10:23:15 +1100

    Backup home folders for apps

---
+++ ansible/files/backups/pack-app-home-folders.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+VM=${1}
+echo "Packing application home folders $(date)"
+
+for file in $(find /data/ -maxdepth 1 -name "*_home"); do
+  echo "File $file"
+  tar -czvf /opt/backups/$(basename $file)-$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tar.gz
+done
