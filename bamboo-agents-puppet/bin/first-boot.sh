b9f497f8f6ee093181fb15cc33807273e3f961ba
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-12-02 18:25:41 +1100

    Calling puppet from ansible for bamboo-agents

--- bamboo-agents-puppet/bin/first-boot.sh
+++ bamboo-agents-puppet/bin/first-boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-GUARD_FILE="/tmp/first_boot.log"
+GUARD_FILE="/var/run/first_boot_puppet.lock"
 
 if [ -f "$GUARD_FILE" ]; then
 	echo "Not first boot; skipping basic installation.... "
@@ -9,10 +9,11 @@ fi
 
 echo "Installing basic dependencies...."
 echo 'Installing git, ruby-dev and puppet'
-apt-get update
 apt-get -q -y install git
 apt-get install -y ruby-dev make
 apt-get install -y -o Dpkg::Options::="--force-confold" puppet
 apt-get -y autoremove
+
+gem install librarian-puppet
 
 touch $GUARD_FILE
--- bamboo-agents-puppet/bin/run-puppet.sh
+++ bamboo-agents-puppet/bin/run-puppet.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /etc/puppet/
-git pull --rebase
 librarian-puppet install
 puppet apply /etc/puppet/manifests/site.pp
