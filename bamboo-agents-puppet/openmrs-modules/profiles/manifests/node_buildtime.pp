cc52519cec75ae2c4dd5708e9d6acc7e3a5cfae5
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-12-03 18:13:58 +1100

    Upgrade npm

--- bamboo-agents-puppet/openmrs-modules/profiles/manifests/node_buildtime.pp
+++ bamboo-agents-puppet/openmrs-modules/profiles/manifests/node_buildtime.pp
@@ -2,10 +2,16 @@ class profiles::node_buildtime {
   class { 'nodejs':
     repo_url_suffix           => '6.x',
     nodejs_package_ensure     => 'latest',
-    legacy_debian_symlinks    => false, 
+    legacy_debian_symlinks    => false,
+    npm_package_name          => false,
   }
-
+  ->
   package { 'bower':
+    provider => 'npm',
+  }
+  ->
+  package { 'npm':
+    ensure   => '5.5.1',
     provider => 'npm',
   }
 }
