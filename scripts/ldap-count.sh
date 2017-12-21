f07c173fed282b79b4ef9ca6f1888b405aa4f42b
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2017-12-21 23:38:42 +1100

    Removing xsede machines

--- scripts/ldap-count.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ldapsearch -x -W -LLL -D cn=admin,dc=openmrs,dc=org -b ou=users,dc=openmrs,dc=org uid=* uid | grep uid: | wc -l
--- scripts/ldap-maintenance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This script checkpoints the ldab db and then archives the log files.
-# Use && to ensure checkpoint is successful before archiving.
-# Runs this nightly from crontab.
-db5.3_checkpoint -1 -v -h /opt/id/data/ldap/database && db5.3_archive -d -v -h /opt/id/data/ldap/database
