dbd9eb99381cf6334c972f14e5e1a7db35ad3242
Sparsha Dotel <sparshadotel@gmail.com>
2018-01-28 17:03:18 +0545

    ITSM-4029 Fix bamboo home Backup

--- ansible/files/backups/pack-app-home-folders.sh
+++ ansible/files/backups/pack-app-home-folders.sh
@@ -8,5 +8,5 @@ echo "Packing application home folders $(date)"
 for file in $(find /data/ -maxdepth 1 -name "*_home"); do
   echo "Folder $file"
   (cd $file
-  tar --exclude="./caches" -zcvf /opt/backups/$(basename $file)-$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tar.gz .)
+  tar --exclude="./artifacts" -zcvf /opt/backups/$(basename $file)-$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tar.gz .)
 done
