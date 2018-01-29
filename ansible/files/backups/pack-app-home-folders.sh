bdf9e5451e2f8e0943254a4ca23d707acac3e0c8
Sparsha Dotel <sparshadotel@gmail.com>
2018-01-29 11:15:52 +0545

    ITSM-4029 Modified backup script to ignore artifacts folder during bamboo backup

--- ansible/files/backups/pack-app-home-folders.sh
+++ ansible/files/backups/pack-app-home-folders.sh
@@ -7,6 +7,12 @@ echo "Packing application home folders $(date)"
 
 for file in $(find /data/ -maxdepth 1 -name "*_home"); do
   echo "Folder $file"
-  (cd $file
-  tar --exclude="./artifacts" -zcvf /opt/backups/$(basename $file)-$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tar.gz .)
+  if [ $file = "/data/bamboo_home" ]
+  then
+    (cd $file
+     tar --exclude="./artifacts" -zcvf /opt/backups/$(basename $file)-$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tar.gz .)
+  else
+    (cd $file
+    tar -czvf /opt/backups/$(basename $file)-$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tar.gz .)
+  fi
 done
