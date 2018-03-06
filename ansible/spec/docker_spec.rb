d3e1d961e0e380391f290fce49c0c28d46275864
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2018-03-06 22:40:30 +1100

    Trying to fix vagrant

--- ansible/spec/docker_spec.rb
+++ ansible/spec/docker_spec.rb
@@ -2,13 +2,13 @@ require_relative 'spec_helper'
 
 describe 'docker machine' do
   it 'should have docker command' do
-    expect(command('docker version').stdout).to include('1.13')
+    expect(command('docker version').stdout).to include('1.1')
   end
   it 'should have docker on overlay2' do
     expect(command('docker info').stdout).to include('Storage Driver: overlay2')
   end
   it 'should have docker-compose command' do
-    expect(command('docker-compose version').stdout).to include('1.10.0')
+    expect(command('docker-compose version').stdout).to include('1.19.0')
   end
   it 'should have demo running' do
     expect(command('cd /root/docker/demo && docker-compose ps').stdout).to include('Up')
