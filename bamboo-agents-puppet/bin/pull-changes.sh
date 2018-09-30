0e258c309fd91c1aa0428e536a0de996db9652a4
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2018-09-30 22:37:04 +1000

    Decrypt files inside puppet

--- bamboo-agents-puppet/bin/pull-changes.sh
+++ bamboo-agents-puppet/bin/pull-changes.sh
@@ -6,5 +6,7 @@ set -x
 
 rm -rf /tmp/puppet
 GIT_SSH_COMMAND='ssh -i /root/.ssh/github_id_rsa' git clone --depth 1 git@github.com:openmrs/openmrs-contrib-itsmresources.git /tmp/puppet
+cd /tmp/puppet
+GNUPGHOME="/root/ansible-gnupg" git-crypt unlock
 rm -rf /etc/openmrs-puppet
 mv /tmp/puppet/bamboo-agents-puppet /etc/openmrs-puppet
