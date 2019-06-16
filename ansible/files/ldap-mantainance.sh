ea7e0f8a2532612ac664b0e67dd66b3bb0918b35
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2019-06-16 11:35:38 +1000

    ITSM-4188: removing terrible hack used to keep old ldap

--- ansible/files/ldap-mantainance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eux
-# This script checkpoints the ldab db and then archives the log files.
-# Use && to ensure checkpoint is successful before archiving.
-# Runs this nightly from crontab.
-db5.3_checkpoint -1 -v -h /data/docker/volumes/ldap_database/_data/ && db5.3_archive -d -v -h /data/docker/volumes/ldap_database/_data/
