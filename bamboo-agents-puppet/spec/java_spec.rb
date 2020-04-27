fe8c91100ad3497f545dcbab07b90dee84207839
cintiadr <cintiadrio@gmail.com>
2020-04-27 19:41:06 +1000

    ITSM-4265: latest version that works with our setup
    https://issues.apache.org/jira/browse/WAGON-590

--- bamboo-agents-puppet/spec/java_spec.rb
+++ bamboo-agents-puppet/spec/java_spec.rb
@@ -8,7 +8,7 @@ describe 'java builder machine' do
     expect(command('/usr/lib/jvm/java-1.7.0-openjdk-amd64/jre/bin/java -version').stderr).to include('1.7.0')
   end
   it 'should have mvn command' do
-    expect(command('mvn3 --version').stdout).to include('3.6.3')
+    expect(command('mvn3 --version').stdout).to include('3.3.9')
   end
   it 'should have mvn creds' do
     expect(command('fgrep "<password>" /home/bamboo-agent/.m2/settings.xml | uniq -c').stdout).to include('4       <password>amazingMavenTestPassword</password>')
