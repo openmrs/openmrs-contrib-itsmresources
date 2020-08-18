30d711fb074ff3feb343a67c6fd1426963284bb5
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2020-08-18 21:26:47 +1000

    Moving transifex to python3

--- bamboo-agents-puppet/openmrs-modules/profiles/manifests/python_buildtime.pp
+++ bamboo-agents-puppet/openmrs-modules/profiles/manifests/python_buildtime.pp
@@ -1,5 +1,5 @@
 class profiles::python_buildtime {
-  package { ['python-pip', 'python-virtualenv']:
+  package { ['python-pip', 'python-virtualenv', 'python3-pip']:
     ensure  => present,
   }
 }
--- bamboo-agents-puppet/openmrs-modules/profiles/manifests/transifex.pp
+++ bamboo-agents-puppet/openmrs-modules/profiles/manifests/transifex.pp
@@ -3,11 +3,11 @@ class profiles::transifex (
 ){
   package { 'pyOpenSSL':
     ensure   => '17.5.0',
-    provider => 'pip',
+    provider => 'pip3',
   } ->
   package { 'transifex-client':
     ensure  => '0.12.5',
-    provider => 'pip',
+    provider => 'pip3',
   }
   bamboo_agent_home::file { '.transifexrc':
     content => template('profiles/transifex/dot-transifexrc.erb'),
