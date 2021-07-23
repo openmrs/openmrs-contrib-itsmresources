9b21b7b20608fdd01245ae23590d7c5f9e8940b4
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-07-23 12:22:26 +0200

    Upgrading docker on bamboo agents to 20.10.7

--- bamboo-agents-puppet/openmrs-modules/profiles/manifests/docker.pp
+++ bamboo-agents-puppet/openmrs-modules/profiles/manifests/docker.pp
@@ -5,7 +5,7 @@ class profiles::docker (
     docker_users    => $users,
     dns             => ['8.8.8.8','8.8.4.4'],
     iptables        => true,
-    version         => '18.06.1~ce~3-0~ubuntu',
+    version         => '20.10.7~3-0~ubuntu',
   }
   class { 'docker::compose':
     ensure  => 'present',
