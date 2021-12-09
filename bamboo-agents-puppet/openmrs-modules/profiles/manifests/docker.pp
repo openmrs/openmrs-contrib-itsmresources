be8b468952b8db3fc8e15d95deacb9461a6e4f1b
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2021-12-09 13:46:57 +0100

    Commenting out multi-arch docker setup until kernel upgraded

--- bamboo-agents-puppet/openmrs-modules/profiles/manifests/docker.pp
+++ bamboo-agents-puppet/openmrs-modules/profiles/manifests/docker.pp
@@ -26,18 +26,18 @@ class profiles::docker (
     version => '1.21.2',
   }
   #Enable multi-arch builds (not working until kernel upgraded to 4.8 or newer)
-  exec { "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes":
-    path         => ["/usr/bin", "/usr/sbin"],
-    subscribe    => [
-                  Class['::docker'],
-               ],
-    refreshonly => true,
-  }
-  exec { "docker buildx create --name custom && docker buildx use custom":
-    path         => ["/usr/bin", "/usr/sbin"],
-    subscribe    => [
-                  Class['::docker'],
-               ],
-    refreshonly => true,
-  }
+  #exec { "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes":
+  #  path         => ["/usr/bin", "/usr/sbin"],
+  #  subscribe    => [
+  #                Class['::docker'],
+  #             ],
+  #  refreshonly => true,
+  #}
+  #exec { "docker buildx create --name custom && docker buildx use custom":
+  #  path         => ["/usr/bin", "/usr/sbin"],
+  #  subscribe    => [
+  #                Class['::docker'],
+  #             ],
+  #  refreshonly => true,
+  #}
 }
