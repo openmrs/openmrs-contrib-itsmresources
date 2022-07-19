8365cb2cf0795e5e73297b12044795306112e2ef
rkorytkowski <rafal.korytkowski@gmail.com>
2022-07-20 00:09:18 +0200

    Follow up on multiarch docker

--- bamboo-agents-puppet/openmrs-modules/profiles/manifests/docker.pp
+++ bamboo-agents-puppet/openmrs-modules/profiles/manifests/docker.pp
@@ -14,21 +14,21 @@ class profiles::docker (
     target => '/data/docker',
     force  => true,
     backup => false,
-  } ->
+  }
   #Enable multi-arch builds
   archive { '/usr/local/lib/docker/cli-plugins/docker-buildx':
     ensure => present,
     source => 'https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-amd64',
     user   => 'root',
-  } ->
+  } ~>
   file { '/usr/local/lib/docker/cli-plugins/docker-buildx':
     ensure  => file,
     mode    => '0755'
-  } ->
+  } ~>
   exec { "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes":
     path         => ["/usr/bin", "/usr/sbin"],
     user         => $bamboo_user,
-  } ->
+  } ~>
   exec { "docker buildx create --name cibuilder --use":
     path         => ["/usr/bin", "/usr/sbin"],
     user        => $bamboo_user,
