17e9d235a83f693012e50cced9e0745fd0889568
rkorytkowski <rafal.korytkowski@gmail.com>
2023-01-05 16:21:11 +0100

    Adding cleanup before building script for bamboo predator

---
+++ bamboo-agents-puppet/openmrs-modules/profiles/files/build_helper_scripts/scripts/cleanup-before-building.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Stopping docker containers"
+docker ps -q | xargs docker kill
+
+echo "Removing all docker containers not used in the last 48 hours"
+docker system prune -af --filter until=48h
+
+echo "Removing all unused docker volumes except for m2-repo"
+docker volume ls -q | grep -v 'm2-repo' | xargs docker volume rm -f
