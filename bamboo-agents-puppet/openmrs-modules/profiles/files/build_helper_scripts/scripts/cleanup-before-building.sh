16a3b29c3e0b3822acd73a544089f5f4ac7b262f
rkorytkowski <rafal.korytkowski@gmail.com>
2023-01-09 15:24:07 +0100

    Do not stop moby/buildkit container

--- bamboo-agents-puppet/openmrs-modules/profiles/files/build_helper_scripts/scripts/cleanup-before-building.sh
+++ bamboo-agents-puppet/openmrs-modules/profiles/files/build_helper_scripts/scripts/cleanup-before-building.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Stopping docker containers"
-docker ps -q | xargs docker kill
+echo "Stopping docker containers except for moby/buildkit"
+docker ps -a | grep -v "moby/buildkit" | awk 'NR>1 {print $1}' | xargs docker kill
 
 echo "Removing all docker containers not used in the last 48 hours"
 docker system prune -af --filter until=48h
