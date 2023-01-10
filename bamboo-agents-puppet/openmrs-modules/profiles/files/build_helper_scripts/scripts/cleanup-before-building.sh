70e910934297ebfbb5ebec0866c61aebebb9f6e0
rkorytkowski <rafal.korytkowski@gmail.com>
2023-01-10 11:58:14 +0100

    Remove only unused docker volumes

--- bamboo-agents-puppet/openmrs-modules/profiles/files/build_helper_scripts/scripts/cleanup-before-building.sh
+++ bamboo-agents-puppet/openmrs-modules/profiles/files/build_helper_scripts/scripts/cleanup-before-building.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "Stopping docker containers except for moby/buildkit"
+echo "Stopping all docker containers except for moby/buildkit"
 docker ps -a | grep -v "moby/buildkit" | awk 'NR>1 {print $1}' | xargs docker kill
 
 echo "Removing all docker containers not used in the last 48 hours"
 docker system prune -af --filter until=48h
 
 echo "Removing all unused docker volumes except for m2-repo"
-docker volume ls -q | grep -v 'm2-repo' | xargs docker volume rm -f
+docker volume ls -q -f dangling=true | grep -v 'm2-repo' | xargs docker volume rm -f
