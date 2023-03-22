632936d905ab480771b31799b98a53944772aa5b
Ian <ian_bacher@brown.edu>
2023-03-22 13:54:02 -0400

    Attempt to update transifex CLI to new version

--- bamboo-agents-puppet/openmrs-modules/profiles/manifests/transifex.pp
+++ bamboo-agents-puppet/openmrs-modules/profiles/manifests/transifex.pp
@@ -1,20 +1,29 @@
 class profiles::transifex (
   $transifex_token,
 ){
-  package { ['libffi-dev']:
-    ensure  => present,
-  } ->
-  package { 'cryptography':
-    ensure   => '3.4.8',
-    provider => 'pip3',
-  } ->
-  package { 'pyOpenSSL':
-    ensure   => '21.0.0',
-    provider => 'pip3',
-  } ->
-  package { 'transifex-client':
-    ensure  => '0.12.5',
-    provider => 'pip3',
+  exec { 'make tx dir' :
+    command => "mkdir -p /opt/tx",
+    cwd     => '/opt',
+    creates => "/opt/tx",
+  } ~>
+  archive { '/tmp/tx.tar.gz':
+    ensure => present,
+    source => "https://github.com/transifex/cli/releases/download/1.6.5/tx-linux-amd64.tar.gz",
+    user   => 0,
+    group  => 0,
+    require => Package['unzip']
+  } ~>
+  exec { 'extract tx cli' :
+    command => "/usr/bin/tar xvzf /tmp/tx.tar.gz",
+    cwd     => '/opt/tx',
+    creates => "/opt/tx",
+  } ~>
+  exec { 'link tx cli' :
+    command => "/usr/bin/ln -s /opt/tx/tx /usr/local/bin/tx".
+    creates => "/usr/local/bin/tx",
+  } ~>
+  exec { 'remove tx archive' :
+    command => "rm /tmp/tx.tar.gx"
   }
   bamboo_agent_home::file { '.transifexrc':
     content => template('profiles/transifex/dot-transifexrc.erb'),
