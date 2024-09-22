507a2f93c41142a6ae757757456bb33257cc9a32
Cintia Del Rio <cintia@delrio.im>
2024-09-22 12:24:20 +1000

    INFRA-41: changing permissions of private key when doing docker

--- ansible/files/restart-id.sh
+++ ansible/files/restart-id.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euv
+
 # This script stops, cleans and starts the openLDAP container after a letsencrypt certificate renewal.
 
 
@@ -16,15 +18,25 @@ log() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $msg" >> $logfile
 }
 
+# Docker is running with user 1001
+log "Making sure private key private key is accessible by ldap docker user"
+find /etc/letsencrypt/archive -name "priv*" | xargs chmod a+r
+find /etc/letsencrypt/archive -name "priv*" | xargs ls -la
+
+
 # Log the start of the script
 log "Running post-renewal hook."
 
 # Change to open ldap directory and restart using the docker-compose file there
-cd /root/docker/ldap-stg/
-DOCKER_OUTPUT=$(docker-compose down && docker-compose up -d --force-recreate 2>&1)
+for f in /root/docker/ldap*; do
+    echo "Running docker compose recreate for $f"
+    cd $f
+    DOCKER_OUTPUT=$(docker-compose down && docker-compose up -d --force-recreate 2>&1)
 
-# Log the output of the docker command
-log "Docker command output: $DOCKER_OUTPUT"
+    # Log the output of the docker command
+    log "Docker command output: $DOCKER_OUTPUT"
+done
+
 
 # Log the completion of the script
 log "Post-renewal hook completed."
