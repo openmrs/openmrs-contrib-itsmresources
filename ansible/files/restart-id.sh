ab4eebe3ae8c476af78c601b11f04f36b34b756b
beatyconsultancy <53967681+beatyconsultancy@users.noreply.github.com>
2024-09-22 03:42:21 +0100

    INFRA-31: new script when letsencrypt renewals
    * Added script for post letsencrypt hook, to restart openldap inm order to pull in the newest certificate.
    
    * INFRA-41: changing permissions of private key when doing docker
    
    * INFRA-31: moving restart id to its own group
    
    ---------
    
    Co-authored-by: Cintia Del Rio <cintia@delrio.im>

---
+++ ansible/files/restart-id.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euv
+
+# This script stops, cleans and starts the openLDAP container after a letsencrypt certificate renewal.
+
+
+# Define our log file
+logfile=/var/log/letsencrypt/renew_hook.log
+
+# Make sure the file exists and is writeable
+touch $logfile
+chmod 644 $logfile
+
+# Define a function to log with a timestamp
+log() {
+    local msg="$1"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $msg" >> $logfile
+}
+
+# Docker is running with user 1001
+log "Making sure private key private key is accessible by ldap docker user"
+find /etc/letsencrypt/archive -name "priv*" | xargs chmod a+r
+find /etc/letsencrypt/archive -name "priv*" | xargs ls -la
+
+
+# Log the start of the script
+log "Running post-renewal hook."
+
+# Change to open ldap directory and restart using the docker-compose file there
+for f in /root/docker/ldap*; do
+    log "Running docker compose recreate for $f"
+    cd $f
+    DOCKER_OUTPUT=$(docker-compose down && docker-compose up -d --force-recreate 2>&1)
+
+    # Log the output of the docker command
+    log "Docker command output: $DOCKER_OUTPUT"
+done
+
+
+# Log the completion of the script
+log "Post-renewal hook completed."
